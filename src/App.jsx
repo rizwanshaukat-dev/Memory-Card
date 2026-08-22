86b74261b15ee98fc92990cc94f2767fc036e671
Rizwan-dev <255385401+rizwanshaukat-dev@users.noreply.github.com>
2026-08-22 23:00:22 +0500

    Implement click-handler and Score & Best-Score logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [clickedIds,setClickedIds]=useState([])
+  const [score,setScore]=useState(0);
+  const [bestScore,setBestScore]=useState(0);
   useEffect(() => {
     async function getCharacters() {
       const response = await fetch("https://rickandmortyapi.com/api/character");
@@ -10,12 +13,22 @@ function App() {
     }
     getCharacters()
   }, []);
-
+  function handleClick(event){
+    if(!clickedIds.includes(event.currentTarget.dataset.id)){
+      setClickedIds([...clickedIds,event.currentTarget.dataset.id]);
+      setScore(score=>score +1);
+    }
+    else{
+      setBestScore(score>bestScore?score:bestScore)
+      setScore(0);
+      setClickedIds([]);
+    }
+  }
   return (
     <div className="card-container">
       {
         characters.map((character) => {
-          return <div className="card" key={character.id}>
+          return <div className="card" key={character.id} data-id={character.id} onClick={handleClick}>
             <img src={character.image} alt="character-image" />
             <strong>{character.name}</strong>
           </div>
